8f25268ce01d5ce58aa81c30b68962099dc85235
Yves Botshimbo Luana <ybidotsh@gmail.com>
2021-08-05 22:46:19 -0300

    Primeiro commit

---
+++ prompt.js
@@ -0,0 +1,30 @@
+const nome = prompt ('Nome');
+console.log('nome digitado =', nome);
+
+const sobrenome = prompt ('Sobrenome');
+console.log('nome digitado =', sobrenome);
+
+const sexo = prompt ('Sexo');
+console.log('nome digitado =', sexo);
+
+const idade = prompt ('Idade');
+console.log('nome digitado =', idade);
+
+const corpreferida = prompt ('Cor preferida');
+console.log('nome digitado =', corpreferida);
+
+const nome2 = prompt ('Nome');
+console.log('nome digitado =', nome2);
+
+const sobrenome2 = prompt ('Sobrenome');
+console.log('nome digitado =', sobrenome2);
+
+const sexo2 = prompt ('Sexo');
+console.log('nome digitado =', sexo2);
+
+const idade2 = prompt ('Idade');
+console.log('nome digitado =', idade2);
+
+const corpreferida2 = prompt ('Cor preferida');
+console.log('nome digitado =', corpreferida2);
+
